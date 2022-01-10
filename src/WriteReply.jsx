2baedf8da1a74c53c96e9175fe05aa20b5d9fd20
soewaiyanag <soewaiyanag@gmail.com>
2022-01-10 12:05:14 +0630

    responsive WriteReply

--- src/WriteReply.jsx
+++ src/WriteReply.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const WriteReply = (props) => {
   return (
     <div
-      className="grid grid-cols-[auto_1fr_auto] gap-4
-                    bg-white shadow-sm -mt-2 p-4"
+      className="grid gap-4 grid-cols-2
+                bg-white shadow-sm -mt-2 p-4
+                sm:grid-cols-[auto_1fr_auto]"
     >
       <img className="h-10" src={props.avatar} alt="avator" />
       <textarea
         className="rounded px-5 py-2 min-h-[5rem]
-                    resize-none text-base border-2"
+                    resize-none text-base border-2
+                    col-span-2 row-start-1
+                    sm:col-span-1 sm:col-start-2 sm:row-start-1"
         type="text"
         name="write_reply"
         id="write_reply"
