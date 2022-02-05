ea837e8a60a8466edfade0cff90a8c6a36d53b63
soewaiyanag <soewaiyanag@gmail.com>
2022-02-05 13:11:25 +0630

    able to edit content in nested comment

--- src/function/index.js
+++ src/function/index.js
@@ -11,3 +11,16 @@ export const removeFromComments = (comments, id) => {
     return true;
   });
 };
+
+// search nested array of objects by id using recursion and if found edit the content
+export const editComment = (comments, id, content) => {
+  return comments.map((comment) => {
+    if (comment.id === id) {
+      comment.content = content;
+    }
+    if ("replies" in comment) {
+      comment.replies = editComment(comment.replies, id, content);
+    }
+    return comment;
+  });
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,75 +1,6 @@
 import newComment from "../newComment";
-import { removeFromComments } from "../function";
-
-const initialState = {
-  currentUser: {
-    image: {
-      png: "./images/avatars/image-juliusomo.png",
-    },
-    username: "juliusomo",
-  },
-  comments: [
-    {
-      id: 1,
-      content:
-        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: "1 month ago",
-      score: 12,
-      user: {
-        image: {
-          png: "./images/avatars/image-amyrobson.png",
-        },
-        username: "amyrobson",
-      },
-      replies: [],
-    },
-    {
-      id: 2,
-      content:
-        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: "2 weeks ago",
-      score: 5,
-      user: {
-        image: {
-          png: "./images/avatars/image-maxblagun.png",
-        },
-        username: "maxblagun",
-      },
-      replies: [
-        {
-          id: 3,
-          content:
-            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: "1 week ago",
-          score: 4,
-          replyingTo: "maxblagun",
-          user: {
-            image: {
-              png: "./images/avatars/image-ramsesmiron.png",
-            },
-            username: "ramsesmiron",
-          },
-          replies: [],
-        },
-        {
-          id: 4,
-          content:
-            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: "2 days ago",
-          score: 2,
-          replyingTo: "ramsesmiron",
-          user: {
-            image: {
-              png: "./images/avatars/image-juliusomo.png",
-            },
-            username: "juliusomo",
-          },
-          replies: [],
-        },
-      ],
-    },
-  ],
-};
+import { removeFromComments, editComment } from "../function";
+import initialState from "../initialState";
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -82,32 +13,7 @@ const reducer = (state = initialState, action) => {
     case "UPDATE_COMMENT":
       return {
         ...state,
-        comments: state.comments.map((cmt) => {
-          // if the comment is the one being updated
-          if (cmt.id === action.id) {
-            return {
-              ...cmt,
-              content: action.content,
-            };
-          }
-          if ("replies" in cmt) {
-            return {
-              ...cmt,
-              // if the reply is being updated
-              replies: cmt.replies.map((reply) => {
-                if (reply.id === action.id) {
-                  return {
-                    ...reply,
-                    content: action.content,
-                  };
-                }
-                return reply;
-              }),
-            };
-          }
-          // if the comment is not being updated then return the comment
-          return cmt;
-        }),
+        comments: editComment(state.comments, action.id, action.content),
       };
 
     case "SEND_REPLY":
